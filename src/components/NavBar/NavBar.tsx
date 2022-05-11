4de06361175250c8f0ab1e55588927efc554a370
Teddy Crane <teddy.crane@slalom.com>
2022-05-11 08:39:06 -0700

    Set up eslint

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,34 @@
+import './NavBar.css';
+import React, { useState } from 'react';
+import { AppBar, Button, IconButton, Menu as NavMenu } from '@material-ui/core';
+import Menu from '@material-ui/icons/Menu';
+
+export const NavBar = (): JSX.Element => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  return (
+    <AppBar>
+      <div className='toolbar-root'>
+        <IconButton
+          className='menu-icon'
+          edge='start'
+          color='inherit'
+          aria-label='menu'
+          onClick={(e) => setAnchorEl(e.currentTarget)}
+        >
+          <Menu />
+        </IconButton>
+        <NavMenu
+          id='simple-menu'
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={() => setAnchorEl(null)}
+        />
+        <Button color='inherit'>Login</Button>
+      </div>
+    </AppBar>
+  );
+};
+
+export default NavBar;
